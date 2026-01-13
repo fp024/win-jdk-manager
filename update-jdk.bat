a5373bab15047d654a06a6c3fd81056ae38307f6
fp024 <aopenasus@naver.com>
2026-01-13 17:11:49 +0900

    완료 후 키입력 대기 코드 삽입

--- update-jdk.bat
+++ update-jdk.bat
@@ -1,2 +1,4 @@
 @echo off
 powershell -ExecutionPolicy Bypass -File "%~dp0update-jdk.ps1" %*
+echo Press Enter key to continue...
+set /p dummyVar=""
--- update-version-props.bat
+++ update-version-props.bat
@@ -1,2 +1,4 @@
 @echo off
 powershell -ExecutionPolicy Bypass -File "%~dp0update-version-props.ps1" %*
+echo Press Enter key to continue...
+set /p dummyVar=""
